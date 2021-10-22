b372a786a409493e299c05f72e683d849217a1e8
Dee Wong <dwang84@sheffield.ac.uk>
2021-10-22 09:26:56 +0100

    Replace the sheffield package

--- lib/sheffield/EasyReader.java
+++ lib/sheffield/EasyReader.java
@@ -16,237 +16,217 @@ import java.io.*;
 
 public class EasyReader extends BufferedReader {
 
-    // instance variables
+	// instance variables
 
-    private boolean endOfFile = false;
-    private boolean startOfFile = true;
-    private char bufferChar = ' ';
+	private boolean endOfFile = false;
+	private boolean startOfFile = true;
+	private char bufferChar = ' ';
 
-    // Constructors
+	// Constructors
 
-    /**
-     * Create a new EasyReader that reads from standard input
-     */
-    public EasyReader() {
-        super(new InputStreamReader(System.in));
-    }
+	/**
+	* Create a new EasyReader that reads from standard input
+	*/
+	public EasyReader() {
+		super(new InputStreamReader(System.in));
+		}
 
-    /**
-     * Create a new EasyReader that reads from a text file
-     * 
-     * @param s the name of the text file
-     */
-    public EasyReader(String s) {
-        super(new InputStreamReader(getFileInputStream(s)));
-    }
+	/**
+	* Create a new EasyReader that reads from a text file
+	* @param s the name of the text file
+	*/
+	public EasyReader(String s) {
+		super(new InputStreamReader(getFileInputStream(s)));
+		}
 
-    // Private methods
+	// Private methods
 
-    private static FileInputStream getFileInputStream(String s) {
-        FileInputStream f = null;
-        try {
-            f = new FileInputStream(s);
-        } catch (IOException e) {
-            error("file not found");
-        }
-        return f;
-    }
+	private static FileInputStream getFileInputStream(String s) {
+		FileInputStream f = null;
+		try {
+			f = new FileInputStream(s);
+			}
+		catch (IOException e) {
+			error("file not found");
+			}
+		return f;
+		}
 
-    private static void error(String s) {
-        System.err.println("Error in EasyReader - " + s);
-        System.err.flush();
-        System.exit(0);
-    }
+	private static void error(String s) {
+		System.err.println("Error in EasyReader - "+s);
+		System.err.flush();
+		System.exit(0);
+		}
 
-    private void prompt(String p) {
-        System.err.print(p);
-        System.err.flush();
-    }
+	private void prompt(String p) {
+		System.err.print(p);
+		System.err.flush();
+		}
 
-    private void getNextChar() {
-        if (!endOfFile) {
-            int c = 0;
-            try {
-                c = read();
-            } catch (IOException e) {
-                error("could not read another character");
-            }
-            if (c == -1)
-                endOfFile = true;
-            else
-                bufferChar = (char) c;
-        }
-    }
+	private void getNextChar() {
+		if (!endOfFile) {
+			int c=0;
+			try {
+				c=read();
+				}
+			catch (IOException e) {
+				error("could not read another character");
+				}
+			if (c==-1)
+				endOfFile=true;
+			else
+				bufferChar=(char)c;
+			}
+		}
 
-    // methods
+	// methods
 
-    /**
-     * Check whether the end of the input stream has been reached
-     * 
-     * @return true if the end of the stream is reached
-     */
-    public boolean eof() {
-        return endOfFile;
-    }
+	/**
+	* Check whether the end of the input stream has been reached
+	* @return true if the end of the stream is reached
+	*/
+	public boolean eof() {
+		return endOfFile;
+		}
 
-    /**
-     * Read a character from the input stream
-     * 
-     * @return a character value
-     */
-    public char readChar() {
-        if (startOfFile) {
-            startOfFile = false;
-            getNextChar();
-        }
-        char ch;
-        do {
-            ch = bufferChar;
-            getNextChar();
-        } while (ch == '\n');
-        return ch;
-    }
+	/**
+	* Read a character from the input stream
+	* @return a character value
+	*/
+	public char readChar() {
+		if (startOfFile) {
+			startOfFile=false;
+			getNextChar();
+			}
+		char ch;
+		do {
+			ch = bufferChar;
+		getNextChar();
+		} while (ch=='\n');
+		return ch;
+		}
 
-    /**
-     * Read a character from the input stream, with a prompt
-     * 
-     * @param s the prompt
-     * @return a character value
-     */
-    public char readChar(String s) {
-        prompt(s);
-        return readChar();
-    }
+	/**
+	* Read a character from the input stream, with a prompt
+	* @param s the prompt
+	* @return a character value
+	*/
+	public char readChar(String s) {
+		prompt(s);
+		return readChar();
+		}
 
-    /**
-     * Read a string from the input stream
-     * 
-     * @return a string value
-     */
-    public String readString() {
-        while (!endOfFile && (bufferChar == ' ' || bufferChar == '\r' || bufferChar == '\n'))
-            getNextChar();
-        String s = "";
-        while (!endOfFile && (bufferChar != '\r' && bufferChar != '\n')) {
-            s += bufferChar;
-            getNextChar();
-        }
-        return s;
-    }
+	/**
+	* Read a string from the input stream
+	* @return a string value
+	*/
+	public String readString() {
+		while (!endOfFile && (bufferChar==' ' || bufferChar=='\r' || bufferChar=='\n'))
+			getNextChar();
+		String s = "";
+		while (!endOfFile && (bufferChar!='\r' && bufferChar!='\n')) {
+			s+=bufferChar;
+			getNextChar();
+			}
+		return s;
+		}
 
-    /**
-     * Read a string from the input stream, with a prompt
-     * 
-     * @return a string value
-     */
-    public String readString(String s) {
-        prompt(s);
-        return readString();
-    }
+	/**
+	* Read a string from the input stream, with a prompt
+	* @return a string value
+	*/
+	public String readString(String s) {
+		prompt(s);
+		return readString();
+		}
 
-    /**
-     * Read a double from the input stream
-     * 
-     * @return a double value
-     */
-    public double readDouble() {
-        double x = 0;
-        try {
-            x = (Double.valueOf(readString())).doubleValue();
-        } catch (Exception e) {
-            error("invalid floating point number");
-        }
-        return x;
-    }
+	/**
+	* Read a double from the input stream
+	* @return a double value
+	*/
+	public double readDouble() {
+		double x = 0;
+		try {
+			x = (Double.valueOf(readString())).doubleValue();
+			}
+		catch (Exception e) {
+			error("invalid floating point number");
+			}
+		return x;
+		}
 
-    /**
-     * Read a double from the input stream, with a prompt
-     * 
-     * @return a double value
-     */
-    public double readDouble(String s) {
-        prompt(s);
-        return readDouble();
-    }
+	/**
+	* Read a double from the input stream, with a prompt
+	* @return a double value
+	*/
+	public double readDouble(String s) {
+		prompt(s);
+		return readDouble();
+		}
 
-    /**
-     * Read a float from the input stream
-     * 
-     * @return a float value
-     */
-    public float readFloat() {
-        float x = 0;
-        try {
-            x = (Float.valueOf(readString())).floatValue();
-        } catch (Exception e) {
-            error("invalid floating point number");
-        }
-        return x;
-    }
+	/**
+	* Read a float from the input stream
+	* @return a float value
+	*/
+	public float readFloat() {
+		float x = 0;
+		try {
+			x = (Float.valueOf(readString())).floatValue();
+			}
+		catch (Exception e) {
+			error("invalid floating point number");
+			}
+		return x;
+		}
 
-    /**
-     * Read a float from the input stream, with a prompt
-     * 
-     * @return a float value
-     */
-    public float readFloat(String s) {
-        prompt(s);
-        return readFloat();
-    }
+	/**
+	* Read a float from the input stream, with a prompt
+	* @return a float value
+	*/
+	public float readFloat(String s) {
+		 prompt(s);
+		 return readFloat();
+		 }
 
-    /**
-     * Read an integer from the input stream
-     * 
-     * @return an integer value
-     */
-    public int readInt() {
-        int x = 0;
-        try {
-            x = (Integer.valueOf(readString())).intValue();
-        } catch (Exception e) {
-            error("invalid integer number");
-        }
-        return x;
-    }
+	/**
+	* Read an integer from the input stream
+	* @return an integer value
+	*/
+	public int readInt() {
+		int x = 0;
+		try {
+			x = (Integer.valueOf(readString())).intValue();
+			}
+		catch (Exception e) {
+			error("invalid integer number");
+			}
+		return x;
+		}
 
-    /**
-     * Read an integer from the input stream, with a prompt
-     * 
-     * @return an integer value
-     */
-    public int readInt(String s) {
-        prompt(s);
-        return readInt();
-    }
+	/**
+	* Read an integer from the input stream, with a prompt
+	* @return an integer value
+	*/
+	public int readInt(String s) {
+		prompt(s);
+		return readInt();
+		}
 
-    /**
-     * Read a boolean from the input stream
-     * 
-     * @return a boolean value
-     */
-    public boolean readBoolean() {
-        char c = readString().charAt(0);
-        return (c == 't' || c == 'T');
-    }
+	/**
+	* Read a boolean from the input stream
+	* @return a boolean value
+	*/
+	public boolean readBoolean() {
+		char c = readString().charAt(0);
+		return (c=='t' || c=='T');
+		}
 
-    /**
-     * Read a boolean from the input stream, with a prompt
-     * 
-     * @return a boolean value
-     */
-    public boolean readBoolean(String s) {
-        prompt(s);
-        return readBoolean();
-    }
-
-    /**
-     * Close the reader
-     */
-    public void close() {
-        try {
-            super.close();
-        } catch (IOException e) {
-            System.out.println("Catch IOException.");
-        }
-    }
-}
+	/**
+	* Read a boolean from the input stream, with a prompt
+	* @return a boolean value
+	*/
+	public boolean readBoolean(String s) {
+		prompt(s);
+		return readBoolean();
+		}
+	}
